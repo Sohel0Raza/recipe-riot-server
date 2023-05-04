97d8708efe76c104037378e556df2c5432af7b21
soheldot <soheldot1239@gmail.com>
2023-05-04 20:49:23 +0600

    modifie some data

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/chefData',(req,res)=>{
     res.send(chefData);
 })
 
+app.get('/chefData/:id',(req,res)=>{
+    const id = req.params.id
+    const myChef = chefData.find(cf => cf.id == id)
+    res.send(myChef);
+})
+
 app.listen(port, ()=>{
     console.log(`Recipe API is running on port: ${port}`);
 });
